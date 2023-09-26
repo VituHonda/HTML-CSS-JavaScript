161272c9ad056bbea021f35630f62752c924ae43
vituhonda <vitorutk@gmail.com>
2023-09-26 08:34:39 -0300

    feat(DomJs): adding studies of DOM

---
+++ DomJs/js/main.js
@@ -0,0 +1,62 @@
+const controle = document.querySelectorAll("[data-controle]")
+const estatistica = document.querySelectorAll("[data-estatistica]")
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (e) => {
+        console.log(e.target.dataset.controle)
+        manipulaDados(e.target.dataset.controle, e.target.parentNode)
+        atualizaEstatisticas(e.target.dataset.peca)
+    }) 
+})
+
+function manipulaDados(operacao, local) {
+
+    const peca = local.querySelector("[data-contador]")
+
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
+    } else {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+function atualizaEstatisticas(peca) {
+
+    estatistica.forEach(e => {
+        e.textContent = parseInt(e.textContent) + pecas[peca][e.dataset.estatistica]
+    });
+
+}
